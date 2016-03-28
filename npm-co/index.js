fbc280a532cc72712453b3842b1e40ed086f7791
bgdsh <bgd_sh@hotmail.com>
2016-03-28 18:30:25 +0800

    小知识点

--- npm-co/index.js
+++ npm-co/index.js
@@ -1,8 +1,30 @@
 var co=require('co');
 co(function* (){
-	return yield Promise.resolve(1);
+	var promise=new Promise(function(resolve,reject){
+		resolve("abc");
+	});
+	return yield promise;
 }).then(function(val){
 	console.log(val);
-},function(err){
-
 });
+
+var fn=co.wrap(function* (val){
+	if(val>10){
+		return yield Promise.resolve(val);
+	}else{
+		return yield Promise.reject(val);
+	}
+});
+fn(10).then(function(val){
+	console.log(val+'合法');
+},function(val){
+	console.log(val+'不合法');
+})
+
+co(function* (){
+	var res=yield {
+		1:Promise.resolve(1),
+		2:Promise.resolve(2)
+	};
+	console.log(res);
+})
