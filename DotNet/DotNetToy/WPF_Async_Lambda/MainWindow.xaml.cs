a37a40f62f20a080a7919fee54f850966777d22f
白国栋 <bgd_sh@hotmail.com>
2016-04-04 02:41:12 +0800

    WPF_Async_Lambda

---
+++ DotNet/DotNetToy/WPF_Async_Lambda/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Async_Lambda
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            startWorkButton.Click += async (sender,e) => {
+                SetGuiValues(false,"开始工作了");
+                await DoSomeWork();
+                SetGuiValues(true, "工作结束了");
+            };
+        }
+        private void SetGuiValues(bool buttonEnabled,string status)
+        {
+            startWorkButton.IsEnabled = buttonEnabled;
+            workStartedTextBlock.Content = status;
+        }
+        private Task DoSomeWork()
+        {
+            return Task.Delay(2500);
+        }
+    }
+}
