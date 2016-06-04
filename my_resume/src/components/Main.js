56da423b3cc9f6af79a1e73c31e76f0db99f57d7
bgdsh <bgd_sh@hotmail.com>
2016-06-04 15:22:49 +0800

    完成工作经历一栏

--- my_resume/src/components/Main.js
+++ my_resume/src/components/Main.js
@@ -4,13 +4,14 @@ import React from 'react';
 import {Carousel } from 'antd';
 import 'antd/dist/antd.css';
 import Profile from './Profile'
+import Experience from './Experience'
 
 class AppComponent extends React.Component {
   render() {
     return (
       <Carousel>
         <div><Profile></Profile></div>
-        <div><h3>2</h3></div>
+        <div><Experience></Experience></div>
         <div><h3>3</h3></div>
         <div><h3>4</h3></div>
       </Carousel>
